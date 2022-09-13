0c9c6428502482d5b942dc07904e812bc5b4a171
Caleb Gore <cgore2001@gmail.com>
2022-09-13 11:22:50 -0500

    login

--- jsgame/src/App.js
+++ jsgame/src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
 import { Welcome } from './components/welcome';
 import { ApplicationViews } from './views/ApplicationViews';
@@ -11,7 +12,7 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
-      {/* <Route path="/login" element={}/> */}
+      <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register />}/>
       <Route path="*" element={
         <Authorized>
