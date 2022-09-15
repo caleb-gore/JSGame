a8043158d875dc5f088727c8251a62b064628b2a
Caleb Gore <cgore2001@gmail.com>
2022-09-15 15:41:27 -0500

    refactored login and registration to single page

--- jsgame/src/components/auth/Login.js
+++ jsgame/src/components/auth/Login.js
@@ -7,19 +7,22 @@ import { loginUser } from "../../managers/AuthManager"
 
 // COMPONENT
 export const Login = () => {
+    const username = useRef()
+    const password = useRef()
     const invalidDialog = useRef()
     const navigate = useNavigate()
 
+
     /* USER OBJECT TO HOLD USERNAME AND PASSWORD 
     UNTIL LOGIN IS COMPLETED */
-    const [userObj, setUserObj] = useState({
-        username: "admin",
-        password: "admin"
-    })
+    // const [userObj, setUserObj] = useState({
+    //     username: "admin",
+    //     password: "admin"
+    // })
 
     /* ARRAY USED TO KEEP TRACK OF "PAGES" OF LOGIN PROMPTS 
     (PAGE IN "TRUE" POSITION IS DISPLAYED) */
-    const [currentPage, setCurrentPage] = useState([true, false])
+    // const [currentPage, setCurrentPage] = useState([true, false])
 
     /* LOGIN USER USING DATA FROM USER OBJ
     SHOW MODAL IF INVALID LOGIN */
@@ -28,15 +31,14 @@ export const Login = () => {
         e.preventDefault()
 
         const user = {
-            username: userObj.username,
-            password: userObj.password
+            username: username.current.value,
+            password: password.current.value
         }
 
         loginUser(user).then(res => {
             if ("valid" in res && res.valid) {
                 console.log(res);
                 localStorage.setItem("u_token", res.token)
-                localStorage.setItem("username", res.username)
                 localStorage.setItem("is_staff", res.is_staff)
                 if (res.is_staff) {
                     navigate("/admin")
@@ -48,71 +50,51 @@ export const Login = () => {
             }
         })
     }
-    // const handleLogin = (e) => {
-    //     e.preventDefault()
 
-    //     return fetch("http://127.0.0.1:8000/login", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Accept": "application/json"
-    //         },
-    //         body: JSON.stringify({
-    //             username: userObj.username,
-    //             password: userObj.password
-    //         })
-    //     })
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             if ("valid" in res && res.valid && "token" in res) {
-    //             }
-    //             else {
-    //             }
-    //         })
-    // }
+    
+    
+    // /* SHOW PAGE 1 */
+    // const page1 = () => {
+    //     return (
+    //         <>
+    //             <h3>
+    //                 Hi There! Don't I recognize you? You look familiar! What was your name?
+    //             </h3>
+    //             <input
+    //                 value={userObj.username}
+    //                 onChange={(e) => {
+    //                     let copy = { ...userObj };
+    //                     copy.username = e.target.value;
+    //                     setUserObj(copy);
+    //                 }}
+    //             />
+    //             <button onClick={()=>{
+    //                 setCurrentPage([false,true])
+    //             }}>next</button>
+    //         </>
+    //     );
+    // };
 
-    /* SHOW PAGE 1 */
-    const page1 = () => {
-        return (
-            <>
-                <h3>
-                    Hi There! Don't I recognize you? You look familiar! What was your name?
-                </h3>
-                <input
-                    value={userObj.username}
-                    onChange={(e) => {
-                        let copy = { ...userObj };
-                        copy.username = e.target.value;
-                        setUserObj(copy);
-                    }}
-                />
-                <button onClick={()=>{
-                    setCurrentPage([false,true])
-                }}>next</button>
-            </>
-        );
-    };
-
-    /* SHOW PAGE 2 */
-    const page2 = () => {
-        return (
-            <>
-                <h3>
-                    Great to see you again, {userObj.username}! What was that password of yours? Gotta make sure you're actually you!
-                </h3>
-                <input
-                    type="password"
-                    value={userObj.password}
-                    onChange={(e) => {
-                        let copy = { ...userObj };
-                        copy.password = e.target.value;
-                        setUserObj(copy);
-                    }}
-                />
-                <button onClick={(e)=>{handleLogin(e)}}>next</button>
-            </>
-        );
-    };
+    // /* SHOW PAGE 2 */
+    // const page2 = () => {
+    //     return (
+    //         <>
+    //             <h3>
+    //                 Great to see you again, {userObj.username}! What was that password of yours? Gotta make sure you're actually you!
+    //             </h3>
+    //             <input
+    //                 type="password"
+    //                 value={userObj.password}
+    //                 onChange={(e) => {
+    //                     let copy = { ...userObj };
+    //                     copy.password = e.target.value;
+    //                     setUserObj(copy);
+    //                 }}
+    //             />
+    //             <button onClick={(e)=>{handleLogin(e)}}>next</button>
+    //         </>
+    //     );
+    // };
 
     return (
         <main className="container--login" style={{ textAlign: "center" }}>
@@ -122,9 +104,32 @@ export const Login = () => {
                 <button className="button--close" onClick={e => invalidDialog.current.close()}>Close</button>
             </dialog>
 
-            {/* PAGES */}
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>DEFAULT GAME NAME TEXT</h1>
+                    <h2>Please sign in</h2>
+                    <fieldset>
+                        <label htmlFor="inputUsername"> Username address </label>
+                        <input ref={username} type="username" id="username" className="form-control" placeholder="Username address" required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputPassword"> Password </label>
+                        <input ref={password} type="password" id="password" className="form-control" placeholder="Password" required />
+                    </fieldset>
+                    <fieldset style={{
+                        textAlign: "center"
+                    }}>
+                        <button className="btn btn-1 btn-sep icon-send" type="submit">Sign In</button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+            </section>
+
+            {/* PAGES
             {currentPage[0] ? page1() : <></>}
-            {currentPage[1] ? page2() : <></>}
+            {currentPage[1] ? page2() : <></>} */}
         </main>
     )
 }
