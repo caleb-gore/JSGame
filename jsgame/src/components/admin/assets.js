931cc2c9d7ecb98f8dff82dcd313a49ec34da2b0
Caleb Gore <cgore2001@gmail.com>
2022-09-15 14:53:59 -0500

    admin portal and asset mgr

--- jsgame/src/components/admin/assets.js
+++ jsgame/src/components/admin/assets.js
@@ -1,71 +1,160 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { deleteAsset, getAssets, saveAsset } from "../../managers/AssetManager";
 
 export const Assets = () => {
+    const navigate = useNavigate();
     const [newAsset, updateNewAsset] = useState({
-        name:"",
-        width:"",
-        height:"",
+        name: "",
+        width: "",
+        height: "",
         file: "",
-        type: ""
-
-    })
+        type: "",
+    });
+    const [assets, setAssets] = useState([]);
+    const categories = [
+        "background",
+        "trophy",
+        "character",
+        "enemy",
+        "collectable",
+    ];
+    useEffect(() => {
+        getAssets().then(setAssets);
+    }, []);
 
     const handleInputChange = (e, inputName) => {
-        const copy = {...newAsset}
-        copy[inputName] = e.target.value
-        updateNewAsset(copy)
-    }
+        const copy = { ...newAsset };
+        copy[inputName] = e.target.value;
+        updateNewAsset(copy);
+    };
+
+    const handleFileSelect = (e) => {
+        getBase64(e.target.files[0], (base64ImageString) => {
+            const copy = { ...newAsset };
+            copy.file = base64ImageString;
+            updateNewAsset(copy);
+        });
+    };
+
+    const getBase64 = (file, callback) => {
+        const reader = new FileReader();
+        reader.addEventListener("load", () => callback(reader.result));
+        reader.readAsDataURL(file);
+    };
+
+    const listCurrentAssets = () => {
+        return categories.map((cat) => {
+            let categoryAssets = assets.filter((asset) => asset.type === cat);
+            if (!categoryAssets[0]) {
+                return (
+                    <section key={cat}>
+                        <h4>{cat}</h4>
+                        <p>No Assets Uploaded</p>
+                    </section>
+                );
+            } else {
+                return (
+                    <section key={cat}>
+                        <h4 key={cat}>{cat}</h4>
+                        <ul>
+                            {categoryAssets.map((asset) => {
+                                return (
+                                    <li key={`asset--${asset.id}`}>
+                                        <button
+                                            onClick={()=>{
+                                                deleteAsset(asset.id)
+                                                .then(()=>getAssets()
+                                                .then(setAssets))
+                                            }}>delete</button>
+                                        {asset.name}{" "}
+                                        <img
+                                            src={`http://localhost:8000${asset.file}`}
+                                            height="100"
+                                        />
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </section>
+                );
+            }
+        });
+    };
+
     return (
         <>
             <h3>Assets</h3>
+
+            {listCurrentAssets()}
+
             <h3>Upload New Asset</h3>
             <form>
-                <fieldset>
-                <label>
-                    Name: 
-                </label>
-                    <input 
-                        name="name" 
-                        value={newAsset.name} 
-                        onChange={(e)=>handleInputChange(e,"name")} type="text"/>
+                <fieldset key="name">
+                    <label>Name:</label>
+                    <input
+                        required
+                        name="name"
+                        value={newAsset.name}
+                        onChange={(e) => handleInputChange(e, "name")}
+                        type="text"
+                    />
                 </fieldset>
-                <fieldset>
-                <label>
-                    Width (in pixels): 
-                </label>
-                    <input 
+                <fieldset key="width">
+                    <label>Width (in pixels):</label>
+                    <input
+                        required
                         type="number"
                         name="width"
-                        onChange={(e)=>handleInputChange(e,"width")}
+                        onChange={(e) => handleInputChange(e, "width")}
                     />
                 </fieldset>
-                <fieldset>
-                <label>
-                    Height (in pixels): 
-                </label>
-                    <input 
+                <fieldset key="height">
+                    <label>Height (in pixels):</label>
+                    <input
+                        required
                         type="number"
                         name="height"
-                        onChange={(e)=>handleInputChange(e,"height")}
+                        onChange={(e) => handleInputChange(e, "height")}
                     />
                 </fieldset>
-                <fieldset>
-                <label>
-                    type: 
-                </label>
-                    <select name="type" onChange={(e)=>handleInputChange(e,"type")}>
-                        <option>Choose</option>
-                        <option value="background">Background</option>
-                        <option value="trophy">Trophy</option>
-                        <option value="character">Character</option>
-                        <option value="enemy">Enemy</option>
-                        <option value="collectable">Collectable</option>
+                <fieldset key="type">
+                    <label>type:</label>
+                    <select
+                        required
+                        name="type"
+                        onChange={(e) => handleInputChange(e, "type")}
+                    >
+                        <option value={"DEFAULT"} disabled="disabled">
+                            Choose
+                        </option>
+                        {categories.map((cat) => {
+                            return (
+                                <option key={`select--${cat}`} value={cat}>
+                                    {cat}
+                                </option>
+                            );
+                        })}
                     </select>
                 </fieldset>
-                <fieldset>
-                    <input type="file"/>
+                <fieldset key="file">
+                    <input
+                        required
+                        type="file"
+                        onChange={(e) => handleFileSelect(e)}
+                    />
                 </fieldset>
-                <button>Save</button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+
+                        saveAsset(newAsset).then(() =>
+                            getAssets().then(setAssets)
+                        );
+                    }}
+                >
+                    Save
+                </button>
             </form>
         </>
     );
